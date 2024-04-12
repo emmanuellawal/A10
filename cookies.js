402bbc1d87dd3a3ebb4b04a913cc7556d10c7171
Emmanuel Lawal <el344@njit.edu>
2024-04-12 03:04:34 -0400

    fix

--- cookies.js
+++ cookies.js
@@ -11,15 +11,15 @@ document.getElementById('form').addEventListener('submit', (e) => {
 })
 
 
-function setCookie(cname, cvalue, exdays) {
-    const d = new Date();
+function setCookie(key, value, exdays) {
+    let d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     let expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    document.cookie = key + "=" + value + ";" + expires + ";path=/";
   }
   
-  function getCookie(cname) {
-    let name = cname + "=";
+  function getCookie(key) {
+    let name = key + "=";
     let ca = document.cookie.split(';');
     for(let i = 0; i < ca.length; i++) {
       let c = ca[i];
