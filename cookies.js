2fdbfc4deaf3dc42bf6896b6beec0035cb37415b
Emmanuel Lawal <el344@njit.edu>
2024-04-12 02:59:56 -0400

    try

---
+++ cookies.js
@@ -0,0 +1,49 @@
+/*document.getElementById('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    Let key = document.getElementById('key').value
+    Let value = document.getElementById('value').value
+    if (key == "" || value == "") {
+        alert("Please enter all fields")
+    }
+    else {
+        setCookie(key, value, 365)
+    }
+})
+
+function setCookie(key, value, time) {
+
+}*/
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+  
+  function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+      let c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return "";
+  }
+  
+  function checkCookie() {
+    let user = getCookie("username");
+    if (user != "") {
+      alert("Welcome again " + user);
+    } else {
+      user = prompt("Please enter your name:", "");
+      if (user != "" && user != null) {
+        setCookie("username", user, 365);
+      }
+    }
+  }
